fddca74453b960cf4104c96de5d069e68744284b
Vinamra Munot <56406734+vinamramunot-tech@users.noreply.github.com>
2020-06-09 11:47:48 +0530

    Update makingFile.sh

--- makingFiles.sh
+++ makingFiles.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+
+# student list with names in the format "LastName FirstName"
 input="student_list.txt"
+
+courseName=""
 homework_number=""
 
-read -p "Homework Number: " homework_number
+read -p "Course Name: " courseName # "CS123"
+read -p "Homework Number: " homework_number # "2"
+
 clear
 while read line; do
     fname=""
@@ -11,7 +17,7 @@ while read line; do
         nameInFile+="${word}"
         fname+="${word}_"
     done
-    touch "${fname%?}_CS331_HW${homework_number}.txt"
-    echo -e "HW${homework_number} - xxx/100\nName:${fname%?}\n\nPart I (xx/42)\nQ1 (x/4)\nQ2 (x/6)\nQ3 (x/8)\nQ4 (x/4)\nQ5 (x/4)\nQ6 (x/7)\nQ7 (x/3)\nQ8 (x/3)\nQ9 (x/3)\n\nPart II (xx/58)\nQ1 (xx/18)\nQ1-1\nQ1-2\nQ2 (xx/15)\nQ3 (xx/10)\nQ4 (xx/15)" > "${fname%?}_CS331_HW${homework_number}.txt"
+    touch "${fname%?}_${courseName}_HW${homework_number}.txt"
+    echo -e "Contains the string that needs to be inserted in the file" > "${fname%?}_${courseName}_HW${homework_number}.txt"
     
 done < "$input"
