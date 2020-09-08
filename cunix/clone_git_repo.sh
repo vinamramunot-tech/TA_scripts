31debc3661e58a7375d970672223e599ac78290c
vinamramunot-tech <munov01@pfw.edu>
2020-09-08 02:37:29 -0400

    clone, github_text

--- cunix/clone_git_repo.sh
+++ cunix/clone_git_repo.sh
@@ -3,21 +3,32 @@
 GITHUB_USERNAME_LIST="student_github.txt"
 COURSE_NAME="CS232"
 CLONE_ERR="clone_err.txt"
-MAIN_DIR="/Users/vinamramunot/OneDrive - Purdue University Fort Wayne/Fall2020/CS232-Unix/TA_scripts/cunix/student_repo"
+STUDENT_DIR="student_repo"
 
-cd "${MAIN_DIR}"
+# this is to check whether the student directory exists or not
+if [ -d "${MAIN_DIR}" ]
+then
+    cd "${STUDENT_DIR}"
+else
+    mkdir student_repo
+fi
 
-while read line; do
-    for username in $line; do
-        if [ ! -d "${username}_Homework_${COURSE_NAME}" ]
+clear
+
+cd ${STUDENT_DIR}
+cat "$GITHUB_USERNAME_LIST" | while read line 
+do
+    echo $line | while read -a wordarray
+    do
+        if [ ! -d "${wordarray[0]}_Homework_${COURSE_NAME}" ]
         then
-            git clone https://github.com/${username}/Homework_${COURSE_NAME}.git 2> /dev/null
+            git clone ${wordarray[1]} 2> /dev/null
             if [ $? == 0 ]
             then
-                mv Homework_${COURSE_NAME} ${username}_Homework_${COURSE_NAME}
+                mv Homework_${COURSE_NAME} ${wordarray[0]}_Homework_${COURSE_NAME}
             else
-                echo ${username} >> ${CLONE_ERR}
+                echo ${wordarray[0]} >> ${CLONE_ERR}
             fi
         fi
-    done
-done < "$GITHUB_USERNAME_LIST"
+    done 
+done
