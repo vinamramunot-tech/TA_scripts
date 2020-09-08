c91957f3db245b0ac5833687d767f8ee9163e9a7
vinamramunot-tech <munov01@pfw.edu>
2020-09-08 12:56:02 -0400

    fix

--- cunix/check_hw1.sh
+++ cunix/check_hw1.sh
@@ -4,9 +4,9 @@ GITHUB_USERNAME_LIST="student_github.txt"
 COURSE_NAME="CS232"
 CLONE_ERR="clone_err.txt"
 STUDENT_DIR="student_repo"
-FILE_EXISTING=0
-FILE_COMPILED=0
-FILE_RUN=0
+declare -i FILE_EXISTING=0
+declare -i ILE_COMPILED=0
+declare -i FILE_RUN=0
 
 # this is to check whether the student directory exists or not
 if [ -d "${MAIN_DIR}" ]
@@ -31,20 +31,20 @@ do
                 gcc -o helloCS232 helloCS232.c -Wall -Werror
                 if [ $? != 0 ]
                 then                                                                
-                    (( $FILE_COMPILED = 0 ))
+                    (( FILE_COMPILED = 0 ))
                     echo $FILE_COMPILED
                 fi
                 ./helloCS232 > "${OUTPUT}"
                 if [ -s "${OUTPUT}" ] 
                 then
-                    (( $FILE_RUN = 1 ))
+                    (( FILE_RUN = 1 ))
                     echo $FILE_RUN
                 else
-                    (( $FILE_RUN = 0 ))
+                    (( FILE_RUN = 0 ))
                     echo $FILE_RUN
                 fi
             else
-                (( $FILE_EXISTING = 0 )) 
+                (( FILE_EXISTING = 0 )) 
                 echo $FILE_EXISTING
             fi
         else
