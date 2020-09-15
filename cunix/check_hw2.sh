f3f354ae800551f0fe50b31943088025e209b7f9
vinamramunot-tech <munov01@pfw.edu>
2020-09-15 12:07:36 -0400

    update

--- cunix/check_hw2.sh
+++ cunix/check_hw2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 
-TEST_SCRIPT="runSimpleTests.sh"
 OUTPUT_CHECK="primesCheckOutput"
 GITHUB_USERNAME_LIST="student_github.txt"
 COURSE_NAME="CS232"
@@ -13,7 +12,7 @@ HOMEWORK_FOLDER_EXISTING=0
 PRIMES_FILE_EXISTING=0
 PRIMES_FILE_COMPILED=0
 PRIMES_FILE_RUN=0
-TEST_PASSED=0
+TEST_SCORE=0
 
 cd ${STUDENT_DIR}
 echo > ${HW2_RESULT}
@@ -21,64 +20,74 @@ cat "$GITHUB_USERNAME_LIST" | while read line
 do
     echo $line | while read -a wordarray
     do
-        (( HOMEWORK_FOLDER_EXISTING=0 ))
         (( PRIMES_FILE_EXISTING=0 ))
         (( PRIMES_FILE_COMPILED=0 ))
         (( PRIMES_FILE_RUN=0 ))
-        (( TEST_PASSED=0 ))
-        if [ -d "${wordarray[0]}_Homework_${COURSE_NAME}" ]
-        then
-            cd "${wordarray[0]}_Homework_${COURSE_NAME}"
-            if [ -d "hw2" ]
+        (( TEST_SCORE=0 ))
+        cp "${wordarray[0]}_Homework_${COURSE_NAME}/hw2/primes.c" ../hw2_scripts/ 2> /dev/null
+        if [ $? == 0 ]
+        then 
+            (( PRIMES_FILE_EXISTING = 1 ))
+            cd ../hw2_scripts/
+            gcc -o primes primes.c -Wall -Werror -lm -std=c99 2> /dev/null
+            if [ $? == 0 ]
             then
-                (( HOMEWORK_FOLDER_EXISTING=1 ))
-                cd hw2
-                # cp "../../../hw2_scripts/${OUTPUT_CHECK}" "../../../hw2_scripts/${TEST_SCRIPT}" "../../../hw2_scripts/${EXPECTED_OUTPUT}" .
-                
-                if [ -f primes.c ]
-                then
-                    cp primes.c ../../../hw2_scripts/
-                    (( PRIMES_FILE_EXISTING=1 ))
-                    gcc -o primes primes.c -Wall -Werror -lm -std=c99 2> /dev/null
-                    if [ $? != 0 ]
-                    then                                                     
-                        (( PRIMES_FILE_COMPILED=0 ))
-                    else
-                        (( PRIMES_FILE_COMPILED=1 ))
-                        ./${TEST_SCRIPT} > ${OUTPUT} 2> /dev/null
-                        if [ $? == 0 ]
-                        then
-                            if [ -s "${OUTPUT}" ] 
-                            then
-                                (( PRIMES_FILE_RUN=1 ))
-                                DIFF=$(diff "$OUTPUT" "$EXPECTED_OUTPUT" 2> /dev/null 1> /dev/null)
-                                if [ "$DIFF" == "" ]
-                                then
-                                    (( TEST_PASSED=1 ))
-                                else
-                                    (( TEST_PASSED=0 ))
-                                fi
-                            else
-                                (( PRIMES_FILE_RUN=0 ))        
-                            fi
-                        fi
-                        
-                    fi
-                    rm primes 2> /dev/null
-                    rm -R -f primes.dSYM 2> /dev/null
-                    rm ${OUTPUT_CHECK} 2> /dev/null
-                    rm ${TEST_SCRIPT} 2> /dev/null
-                    rm ${EXPECTED_OUTPUT} 2> /dev/null
-                    rm ${OUTPUT} 2> /dev/null     
+                (( PRIMES_FILE_COMPILED = 1 ))
+                ./runSimpleTests.sh > "${OUTPUT}"   
+                if [ $? == 0 ]
+                then        
+                    (( PRIMES_FILE_RUN = 1 ))     
+                    while read tests
+                    do
+                        case $tests in
+                            "Tests successfully passed for bounds 1, 1")
+                                TEST_SCORE=$(( TEST_SCORE + 10 ))
+                                ;;
+                            "Tests successfully passed for bounds 1, 2")
+                                TEST_SCORE=$(( TEST_SCORE + 10 ))
+                                ;;
+                            "Tests successfully passed for bounds 1, 5")
+                                TEST_SCORE=$(( TEST_SCORE + 10 ))
+                                ;;
+                            "Tests successfully passed for bounds 2, 100")
+                                TEST_SCORE=$(( TEST_SCORE + 10 ))
+                                ;;
+                            "Tests successfully passed for bounds 50, 51")
+                                TEST_SCORE=$(( TEST_SCORE + 10 ))
+                                ;;
+                            "Tests successfully passed for bounds 50, 200")
+                                TEST_SCORE=$(( TEST_SCORE + 10 ))
+                                ;;
+                            "Tests successfully passed for bounds 7, 7")
+                                TEST_SCORE=$(( TEST_SCORE + 10 ))
+                                ;;
+                            "Tests successfully passed for bounds 17, 12")
+                                TEST_SCORE=$(( TEST_SCORE + 10 ))
+                                ;;
+                            "Tests successfully passed for bounds 73, 127")
+                                TEST_SCORE=$(( TEST_SCORE + 10 ))
+                                ;;
+                            "Tests successfully passed for bounds 160, 162")
+                                TEST_SCORE=$(( TEST_SCORE + 10 ))
+                                ;;
+                            "Tests successfully passed for bounds 40000, 45000")
+                                TEST_SCORE=$(( TEST_SCORE + 10 ))
+                                ;;
+                            *)
+                                ;;
+                        esac
+                    done < "${OUTPUT}"
                 fi
-                cd ../..
-            else
-                (( HOMEWORK_FOLDER_EXISTING=0 ))   
-                cd ..          
             fi
+            rm -R -f primes.dSYM
+            rm primes.c 2> /dev/null
+            rm output.txt
+            rm primes
+            cd "../${STUDENT_DIR}"
         fi
         echo "${wordarray[0]}" >> ${HW2_RESULT}
-        echo -e "\t\tprimes_file_existing\tprimes_file_compiled\tprimes_file_run\ttests_passed" >> ${HW2_RESULT}
-        echo -e "\t\t\t\t${PRIMES_FILE_EXISTING}\t\t\t\t\t\t\t${PRIMES_FILE_COMPILED}\t\t\t\t\t${PRIMES_FILE_RUN}\t\t\t\t${TEST_PASSED}" >> ${HW2_RESULT}
-    done 
+        echo -e "\t\tprimes_file_existing\tprimes_file_compiled\tprimes_file_run\ttests_score" >> ${HW2_RESULT}
+        echo -e "\t\t\t\t${PRIMES_FILE_EXISTING}\t\t\t\t\t\t\t${PRIMES_FILE_COMPILED}\t\t\t\t\t${PRIMES_FILE_RUN}\t\t\t\t${TEST_SCORE}" >> ${HW2_RESULT}
+
+    done
 done
