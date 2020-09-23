4ee630538fce6709a318e95fe0ee83c849def002
vinamramunot-tech <munov01@pfw.edu>
2020-09-23 12:10:36 -0400

    update

--- cunix/student_repo/check_hw3.sh
+++ cunix/student_repo/check_hw3.sh
@@ -3,12 +3,48 @@
 GITHUB_USERNAME_LIST="student_github.txt"
 COURSE_NAME="CS232"
 OUTPUT="output.txt"
-STUDENT_DIR="student_repo"
+HW3_RESULT="hw_results/hw3_result.txt"
 FILE_EXISTING=0
 FILE_COMPILED=0
-FILE_RUN=0
-HW3_RESULT="hw3_result.txt"
+BOARD_THREE=0
+BOARD_FOUR=0
 
+function createHomeworkResult(){
+    if [ -f "$HW3_RESULT" ]
+    then
+        rm -f "$HW3_RESULT"
+        touch "$HW3_RESULT"
+    else
+        touch "$HW3_RESULT"
+    fi
+}
+
+function copyExpectedResult(){
+    cp ../../../hw3_solution_scripts/log_4.txt .
+    cp ../../../hw3_solution_scripts/log_3.txt .
+    cp ../../../hw3_solution_scripts/3x3.txt .
+    cp ../../../hw3_solution_scripts/4x4.txt .
+}
+
+function run3by3(){
+    ./fifteen 3 < 3x3.txt > /dev/null
+}
+
+function run4by4(){
+    ./fifteen 4 < 4x4.txt > /dev/null
+}
+
+function remove(){
+    rm 3x3.txt
+    rm 4x4.txt
+    rm log_3.txt
+    rm log_4.txt
+    rm fifteen
+    rm log.txt
+}
+
+
+createHomeworkResult
 
 cat "$GITHUB_USERNAME_LIST" | while read line 
 do
@@ -16,39 +52,49 @@ do
     do
         (( FILE_EXISTING = 0 ))
         (( FILE_COMPILED = 0 ))
-        (( FILE_RUN = 0 ))
+        (( BOARD_THREE = 0 ))
+        (( BOARD_FOUR = 0 ))
         if [ -d "${wordarray[0]}_Homework_${COURSE_NAME}" ]
         then
             cd "${wordarray[0]}_Homework_${COURSE_NAME}"
-            if [ -d "hw1" ]
+            if [ -d "hw3" ]
             then
-                (( FILE_EXISTING = 1 ))
-                cd hw1
-                gcc -o helloCS232 helloCS232.c -Wall -Werror 2> /dev/null
-                if [ $? != 0 ]
-                then                                                     
-                    (( FILE_COMPILED = 0 ))
-                else
-                    (( FILE_COMPILED = 1 ))
-                    ./helloCS232 > "${OUTPUT}"
-                    if [ -s "${OUTPUT}" ] 
+                cd hw3
+                if [ -f "fifteen.c" ]
+                then
+                    (( FILE_EXISTING = 1 ))
+                    gcc -o fifteen fifteen.c -Wall -Werror -lm 2> /dev/null
+                    if [ $? == 0 ]
                     then
-                        (( FILE_RUN = 1 ))
-                        rm ${OUTPUT}
-                    else
-                        (( FILE_RUN = 0 ))        
+                        (( FILE_COMPILED = 1 ))
+                        copyExpectedResult
+
+                        run3by3
+                        if cmp -s "log_3.txt" "log.txt"
+                        then
+                            echo "3x3 are equal!"
+                            (( BOARD_THREE = 1 ))
+                        else
+                            echo "3x3 are not equal!"
+                        fi    
+
+                        run4by4
+                        if cmp -s "log_4.txt" "log.txt"
+                        then
+                            echo "4x4 are equal!"
+                            (( BOARD_FOUR = 1 ))
+                        else
+                            echo "4x4 are not equal!"
+                        fi
+                        remove
                     fi
                 fi
-                rm helloCS232
-                cd ../..
-            else
-                (( FILE_EXISTING = 0 ))     
-                cd ..        
+                cd ..
             fi
+            cd ..
         fi
-        
-        echo "${wordarray[0]}" >> ${HW1_RESULT}
-        echo -e "\t\tfile_existing\tfile_compiled\tfile_run" >> ${HW1_RESULT}
-        echo -e "\t\t\t  ${FILE_EXISTING} \t\t\t\t${FILE_COMPILED}\t\t\t${FILE_RUN}" >> ${HW1_RESULT}
-    done 
+        echo "${wordarray[0]}" >> ${HW3_RESULT}
+        echo -e "\t\tfile_existing\tfile_compiled\t\t\tBoard 3\t\t\tBoard 4" >> ${HW3_RESULT}
+        echo -e "\t\t\t  ${FILE_EXISTING} \t\t\t\t${FILE_COMPILED}\t\t\t${BOARD_THREE}\t\t\t${BOARD_FOUR}" >> ${HW3_RESULT}
+    done
 done
--- cunix/student_repo/rm_result.sh
+++ cunix/student_repo/rm_result.sh
@@ -2,9 +2,7 @@
 
 GITHUB_USERNAME_LIST="student_github.txt"
 COURSE_NAME="CS232"
-STUDENT_DIR="student_repo"
 
-cd ${STUDENT_DIR}
 cat "$GITHUB_USERNAME_LIST" | while read line 
 do
     echo $line | while read -a wordarray
@@ -12,16 +10,16 @@ do
         if [ -d "${wordarray[0]}_Homework_${COURSE_NAME}" ]
         then
             cd "${wordarray[0]}_Homework_${COURSE_NAME}"
-            if [ -d "hw2" ]
+            if [ -d "hw3" ]
             then
-                cd hw2
-                rm hw2_result.txt 2> /dev/null
-                rm expectedOutput.txt 2> /dev/null
-                rm primesCheckOutput 2> /dev/null
-                rm runSimpleTests.sh 2> /dev/null
+                cd hw3
+                rm 3x3.txt 2> /dev/null
+                rm 4x4.txt 2> /dev/null
+                rm fifteen 2> /dev/null
+                rm log_3.txt 2> /dev/null
                 rm -R -f primes.dSYM 2> /dev/null
-                rm primes 2> /dev/null
-                cd ../../
+                rm log_4.txt 2> /dev/null
+                cd ..
             fi
             cd ..
         fi
