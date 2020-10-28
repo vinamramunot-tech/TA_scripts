3d9a96c150f7b59bd25bdf2e4633d55d49c68672
vinamramunot-tech <munov01@pfw.edu>
2020-10-27 23:16:22 -0400

    upadte

--- cunix/student_repo/check_project1.sh
+++ cunix/student_repo/check_project1.sh
@@ -8,10 +8,11 @@ EXPECTED_RESULT="expected_results.txt"
 PYTHON_SCRIPTS="getLinks.py"
 RESULT_FILE="results.txt"
 OUTPUT="output.txt"
+TEMP="temp.txt"
 EXECUTABLE="crawler"
-TEST_SCORE=0
-MEMORY_LEAK=0
-MEMORY_BUFFER=""
+TEST=""
+MEMORY_LEAK=""
+
 
 function createProject1ResultFile(){
     if [ -f $1 ]; then rm -f $1 touch $1; else touch $1; fi
@@ -22,13 +23,7 @@ function copyProjectTestFiles(){
 }
 
 function rmTestFiles(){
-    rm $1 && rm $2 && rm $3 && rm $4 && rm $5
-}
-
-function initScore(){
-    (( TEST_SCORE = 0 ))
-    (( MEMORY_LEAK = 0 ))
-    (( MEMORY_BUFFER = "" ))
+    rm $1 && rm $2 && rm $3 && rm $4 && rm $5 && rm $6
 }
 
 createProject1ResultFile $PROJ_RESULT
@@ -37,7 +32,9 @@ while read line
 do
     echo $line | while read -a wordarray
     do
-        initScore
+        TEST=""
+        MEMORY_LEAK=""
+        MEMORY_BUFFER=""
         if [ -d "${wordarray[0]}_Homework_${COURSE_NAME}" ]
         then
             cd "${wordarray[0]}_Homework_${COURSE_NAME}"
@@ -51,27 +48,29 @@ do
                     diff -q $EXPECTED_RESULT $RESULT_FILE
                     if [ $? == 0 ]
                     then
-                        $(( TEST_SCORE = 1 ))
+                        TEST="passed"
                     else
+                        TEST="not-passed"
                         cat $RESULT_FILE > $OUTPUT
                     fi
 
-                    valgrind -q --leak-check=yes --error-exitcode=7 ./crawler "https://users.pfw.edu/chenz/testweb/page_000001.html" 10 899 > $MEMORY_BUFFER
+                    valgrind --leak-check=yes --error-exitcode=7 ./crawler "https://users.pfw.edu/chenz/testweb/page_000001.html" 10 899 1> $TEMP 2> $TEMP
                     if [ $? == 7 ]
                     then
-                        $(( MEMORY_LEAK = 1 ))
+                        MEMORY_LEAK="yes-leaks"
+                        cat $TEMP >> $OUTPUT
                     else
-                        cat $MEMORY_BUFFER >> $OUTPUT
+                        MEMORY_LEAK="no-leaks"
                     fi
                 fi
-                rm ${TEST_SCRIPT} ${EXPECTED_RESULT} ${PYTHON_SCRIPTS} ${EXECUTABLE} ${RESULT_FILE}
+                rm $TEST_SCRIPT $EXPECTED_RESULT $PYTHON_SCRIPTS $EXECUTABLE $RESULT_FILE $TEMP
                 cd ..
             fi
             cd ..
         fi
         echo -n "${wordarray[0]}" >> $PROJ_RESULT
-        echo -e -n "\t $TEST_SCORE" >> $PROJ_RESULT
-        echo -e "\t $MEMORY_LEAK" >> $PROJ_RESULT
+        echo -e -n "\t $TEST" >> $PROJ_RESULT
+        echo -e -n "\t $MEMORY_LEAK" >> $PROJ_RESULT
         echo "" >> $PROJ_RESULT
     done
 done < "$GITHUB_USERNAME_LIST" 
