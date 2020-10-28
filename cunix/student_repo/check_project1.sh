af04c29fa5f5ac11acfcbad2c65ec42c9c078c31
vinamramunot-tech <munov01@pfw.edu>
2020-10-28 00:29:03 -0400

    update

--- cunix/student_repo/check_project1.sh
+++ cunix/student_repo/check_project1.sh
@@ -10,8 +10,9 @@ RESULT_FILE="results.txt"
 OUTPUT="output.txt"
 TEMP="temp.txt"
 EXECUTABLE="crawler"
-TEST=""
-MEMORY_LEAK=""
+COMPILE_ERROR="cannot-be-compiled"
+TEST="test-cannot-be-done"
+MEMORY_LEAK="memory-leak-cannot-be-tested"
 
 
 function createProject1ResultFile(){
@@ -32,9 +33,10 @@ while read line
 do
     echo $line | while read -a wordarray
     do
-        TEST=""
-        MEMORY_LEAK=""
-        MEMORY_BUFFER=""
+        echo "Checking for ${wordarray[0]}...."
+        COMPILE_ERROR="cannot-be-compiled"
+        TEST="test-cannot-be-done"
+        MEMORY_LEAK="memory-leak-cannot-be-tested"
         if [ -d "${wordarray[0]}_Homework_${COURSE_NAME}" ]
         then
             cd "${wordarray[0]}_Homework_${COURSE_NAME}"
@@ -44,31 +46,29 @@ do
                 copyProjectTestFiles ${TEST_SCRIPT} ${EXPECTED_RESULT} ${PYTHON_SCRIPTS}
                 if [ -f "crawler.c" ]
                 then
-                    ./${TEST_SCRIPT}
-                    diff -q $EXPECTED_RESULT $RESULT_FILE
+                    gcc -o crawler crawler.c -Wall -Werror 1>/dev/null 2>&1
                     if [ $? == 0 ]
                     then
-                        TEST="passed"
-                    else
-                        TEST="not-passed"
-                        cat $RESULT_FILE > $OUTPUT
-                    fi
+                        COMPILE_ERROR="compiled"
+                        ./${TEST_SCRIPT}
+                        diff -q $EXPECTED_RESULT $RESULT_FILE
+                        if [ $? == 0 ]; then TEST="passed"; else TEST="not-passed" cat $RESULT_FILE > $OUTPUT; fi
 
-                    valgrind --leak-check=yes --error-exitcode=7 ./crawler "https://users.pfw.edu/chenz/testweb/page_000001.html" 10 899 1> $TEMP 2> $TEMP
-                    if [ $? == 7 ]
-                    then
-                        MEMORY_LEAK="yes-leaks"
-                        cat $TEMP >> $OUTPUT
+                        valgrind --leak-check=yes --error-exitcode=7 ./crawler "https://users.pfw.edu/chenz/testweb/page_000001.html" 10 899 1> $TEMP 2> $TEMP
+                        if [ $? == 7 ]; then MEMORY_LEAK="yes-leaks" cat $TEMP >> $OUTPUT; else MEMORY_LEAK="no-leaks"; fi
+
+                        rm $EXECUTABLE $RESULT_FILE $TEMP
                     else
-                        MEMORY_LEAK="no-leaks"
+                        COMPILE_ERROR="not-compiled"
                     fi
                 fi
-                rm $TEST_SCRIPT $EXPECTED_RESULT $PYTHON_SCRIPTS $EXECUTABLE $RESULT_FILE $TEMP
+                rm $TEST_SCRIPT $EXPECTED_RESULT $PYTHON_SCRIPTS
                 cd ..
             fi
             cd ..
         fi
         echo -n "${wordarray[0]}" >> $PROJ_RESULT
+        echo -e -n "\t $COMPILE_ERROR" >> $PROJ_RESULT
         echo -e -n "\t $TEST" >> $PROJ_RESULT
         echo -e -n "\t $MEMORY_LEAK" >> $PROJ_RESULT
         echo "" >> $PROJ_RESULT
