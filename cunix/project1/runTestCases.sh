8586b3df26b2e64eda3c5aab011c5c55145f274c
vinamramunot-tech <munov01@pfw.edu>
2020-10-28 00:29:30 -0400

    update

--- cunix/project1/runTestCases.sh
+++ cunix/project1/runTestCases.sh
@@ -1,4 +1,3 @@
-gcc -o crawler crawler.c -Wall
 ./crawler "https://users.pfw.edu/chenz/testweb/page_000001.html" 10 899 > results.txt
 ./crawler "https://users.pfw.edu/chenz/testweb/page_000010.html" 5 983 >> results.txt
 ./crawler "https://users.pfw.edu/chenz/testweb/page_000003.html" 4 187 >> results.txt
