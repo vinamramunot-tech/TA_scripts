d9f47e977a5038c042cf6ded5367b2abc568ff8a
vinamramunot-tech <munov01@pfw.edu>
2021-01-04 19:16:00 -0500

    update

--- sql/Lab1/Program.cs
+++ sql/Lab1/Program.cs
@@ -5,58 +5,13 @@ namespace Lab1
 {
     class Program
     {
-
-        static bool checkIfDatabaseExists(SqlConnection _connection)
-        {
-            string query = "SELECT * FROM sys.databases";
-
-            SqlCommand cmd = new SqlCommand(query, _connection);
-
-            //indicate that the database is connected
-            Console.WriteLine("The server is connected! Lets move on!");
-
-            //execute the SQLCommand
-            SqlDataReader dr = cmd.ExecuteReader();
-
-            //check if there are records
-            if (dr.HasRows)
-            {
-                while (dr.Read())
-                {
-                    //display retrieved record (first column only/string value)
-                    if (dr.GetString(0) == "Test")
-                    {
-                        Console.WriteLine("Test Database successfully created and visible!");
-                        dr.Close();
-                        return true;
-                    }
-                }
-            }
-            else
-            {
-                Console.WriteLine("No databases found.");
-                dr.Close();
-            }
-            return false;
-        }
-
         static void tryDatabaseConnection(string connectionString)
         {
-
             try
             {
                 using (SqlConnection conn = new SqlConnection(connectionString))
                 {
                     conn.Open();
-
-                    if (checkIfDatabaseExists(conn))
-                    {
-                        Console.WriteLine("Awesome you have completed the task!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Unfortunately you have not complete the task!");
-                    }
                 }
             }
             catch (Exception ex)
