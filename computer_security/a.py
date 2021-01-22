407e628bf1de600b370dabfeba0c55a9f650f594
vinamramunot-tech <munov01@pfw.edu>
2021-01-21 22:15:20 -0500

    update

--- computer_security/a.py
+++ computer_security/a.py
@@ -1,12 +1,21 @@
-f = open("./students.txt", "+r")
+f = open("./students.txt", "r")
 lines = f.readlines()
+
+data = []
 
 for line in lines:
     name, gitlink = line.split()
     
     last_name, first_name = name.split('_')
     full_name = f'{first_name}_{last_name}'
-
-    f.writelines(full_name)
+    new_line = f'{full_name} {gitlink}'
+    data.append(new_line)
 
 f.close()
+
+with open('./students.txt', 'w') as file:
+    for str in data:
+        file.write(f'{str}\n')
+    
+
+file.close()
