1288cad63c39ba295ce9c66945fc62facf4fc4f1
vinamramunot-tech <munov01@pfw.edu>
2021-01-21 22:02:18 -0500

    update

--- computer_security/a.py
+++ computer_security/a.py
@@ -1,1 +1,12 @@
-f = open("")
+f = open("./students.txt", "+r")
+lines = f.readlines()
+
+for line in lines:
+    name, gitlink = line.split()
+    
+    last_name, first_name = name.split('_')
+    full_name = f'{first_name}_{last_name}'
+
+    f.writelines(full_name)
+
+f.close()
