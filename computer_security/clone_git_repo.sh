f7d300528109ff83f9fc009be8f5019ded314dcc
vinamramunot-tech <munov01@pfw.edu>
2021-01-21 22:40:04 -0500

    update

--- computer_security/clone_git_repo.sh
+++ computer_security/clone_git_repo.sh
@@ -14,13 +14,13 @@ while read line
 do
     echo $line | while read -a wordarray
     do
-        if [ ! -d "${wordarray[0]}_Homework_${COURSE_NAME}" ]
+        if [ ! -d "student_repo/${wordarray[0]}_Homework_${COURSE_NAME}" ]
         then
             echo "Cloning ${wordarray[0]}..."
             git clone ${wordarray[1]} --quiet
             if [ $? == 0 ]
             then
-                mv "${wordarray[2]}" "${wordarray[0]}_Homework_${COURSE_NAME}"
+                mv "${wordarray[2]}" "student_repo/${wordarray[0]}_Homework_${COURSE_NAME}"
             else
                 echo ${wordarray[0]} >> ${CLONE_ERR}
             fi
