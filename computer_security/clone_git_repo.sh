badfa32e3e83136b926ce9df6e21fe74e089b8f0
vinamramunot-tech <munov01@pfw.edu>
2021-01-21 22:20:12 -0500

    update

---
+++ computer_security/clone_git_repo.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+GITHUB_USERNAME_LIST="students.txt"
+COURSE_NAME="CS445"
+CLONE_ERR="clone_err.txt"
+
+function createCloneError(){
+    if [ -f $1 ]; then rm -f $1 touch $1; else touch $1; fi
+}
+
+createCloneError ${CLONE_ERR}
+
+while read line 
+do
+    echo $line | while read -a wordarray
+    do
+        if [ ! -d "${wordarray[0]}_Homework_${COURSE_NAME}" ]
+        then
+            echo "Cloning ${wordarray[0]}..."
+            git clone ${wordarray[1]} --quiet
+            if [ $? == 0 ]
+            then
+                mv "Homework_${COURSE_NAME}" "${wordarray[0]}_Homework_${COURSE_NAME}"
+            else
+                echo ${wordarray[0]} >> ${CLONE_ERR}
+            fi
+            sleep 1s
+        fi
+    done 
+done < "$GITHUB_USERNAME_LIST"
